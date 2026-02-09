89241d591c81dff7e9cc69930a8709c480afbc5b
Vidyarth <vv2466@nyu.edu>
2026-02-08 21:19:39 -0500

    Update routing in App component and enhance NotFound page with Link component

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
